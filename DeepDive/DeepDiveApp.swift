85c478f68f50888b0c5d8869480c2a5059297db6
Mateusz <mateusz.rybczynski@icloud.com>
2024-12-23 23:03:08 +0100

    Initial Commit

---
+++ DeepDive/DeepDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDiveApp.swift
+//  DeepDive
+//
+//  Created by Mateusz Rybczyński on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeepDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
