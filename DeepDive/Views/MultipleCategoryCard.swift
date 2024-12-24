b6399f6ed47ee1e1c08c05e1a64d031c07df9ed0
Mateusz <mateusz.rybczynski@icloud.com>
2024-12-24 11:44:28 +0100

    Create multiple category view

--- DeepDive/Views/MultipleCategoryCard.swift
+++ DeepDive/Views/MultipleCategoryCard.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct MultipleCategoryCard: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @EnvironmentObject var questionStore: QuestionStore
+    
+    var questions: [Question] {
+        questionStore.questions.sorted { $0.id < $1.id }
     }
+    
+    @State private var currentIndex = 0
+    
+    var body: some View {
+        VStack {
+            Text(questions[currentIndex].text)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .gesture(
+            DragGesture()
+                .onEnded { value in
+                    if value.translation.width < 0 { // Przeciągnięcie w lewo
+                        nextQuestion()
+                    } else if value.translation.width > 0 { // Przeciągnięcie w prawo
+                        previousQuestion()
+                    }
+                }
+        )
+        
+    }
+    
+    private func nextQuestion() {
+        print("Next question")
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        }
+    }
+    
+    private func previousQuestion() {
+        print("Previous question")
+        if currentIndex > 0 {
+            currentIndex -= 1
+        }
+    }
+    
 }
 
 #Preview {
     MultipleCategoryCard()
+        .environmentObject(QuestionStore())
 }
