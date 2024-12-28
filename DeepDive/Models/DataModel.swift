b3f33126f8cfed44cd8c58d84abe9721e747348b
Mateusz <mateusz.rybczynski@icloud.com>
2024-12-28 10:13:46 +0100

    Fix questions

--- DeepDive/Models/DataModel.swift
+++ DeepDive/Models/DataModel.swift
@@ -21,8 +21,8 @@ struct Question: Identifiable {
 
 class QuestionStore: ObservableObject {
     @Published var questions: [Question] = [
-        Question(text: "Gdybys miat/miata mozliwość dowiedziec sie prawdy na dowolny temat, jaki by to był?",subtitle: "Co z tymi wszystkimi zaginionymi skarpetami?" ,themeColor: Color.pink, category: "rozgrzewka", image: "skull", backgroundColor: .red.opacity(0.5)),
-        Question(text: "Gdybys miat/miata mozliwość rozmowy z dowolna postacia historyczna, kogo bys wybrał/wybrała i jakie pytanie bys zadał/zadała?",subtitle: "Wyobraz sobie, że masz wehikuł czasu" ,themeColor: Color.pink, category: "rozgrzewka", image: "brain", backgroundColor: .red.opacity(0.5)),
+        Question(text: "Gdybyś miał/miała mozliwość dowiedzieć się prawdy na dowolny temat, jaki by to był?",subtitle: "Co z tymi wszystkimi zaginionymi skarpetami?" ,themeColor: Color.pink, category: "rozgrzewka", image: "brain", backgroundColor: .red.opacity(0.5)),
+        Question(text: "Gdybyś miał/miała mozliwość rozmowy z dowolną postacią historyczną, kogo byś wybrał/wybrała i jakie pytanie byś zadał/zadała?",subtitle: "Wyobraź sobie, że masz wehikuł czasu" ,themeColor: Color.pink, category: "rozgrzewka", image: "brain", backgroundColor: .red.opacity(0.5)),
         Question(text: "Gdybys mógł/mogła doświadczyć zycia w jakiejkolwiek ksiazce lub filmie, które bys wybrat/wybrata i dlaczego?",subtitle: "Stanley Kubrick c z y Tarantino?" ,themeColor: Color.pink, category: "rozgrzewka", image: "brain", backgroundColor: .red.opacity(0.5))
         
     ]
