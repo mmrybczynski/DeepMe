85b54289d2b1473411087a3ddf2662dff613fcc0
Mateusz <mateusz.rybczynski@icloud.com>
2025-08-22 18:46:15 +0200

    Created splash screen

---
+++ DeepMe/Views/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  DeepMe
+//
+//  Created by Mateusz Rybczyński on 22/08/2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
