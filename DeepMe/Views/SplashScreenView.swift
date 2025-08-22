8d16987c2e041eb7a35a482408bd0759ef3d6337
Mateusz <mateusz.rybczynski@icloud.com>
2025-08-22 19:19:14 +0200

    Created first version of splash screen

--- DeepMe/Views/SplashScreenView.swift
+++ DeepMe/Views/SplashScreenView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    
+    @State var labelNumber = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Spacer()
+            
+            Text("Wybierz kategorię")
+                .font(.system(size: labelNumber == 0 ? 28 : 14))
+                .fontWeight(labelNumber == 0 ? .bold : .regular)
+                .animation(.easeInOut, value:labelNumber)
+            
+            Text("Zadawaj pytania")
+                .font(.system(size: labelNumber == 1 ? 28 : 14))
+                .fontWeight(labelNumber == 1 ? .bold : .regular)
+                .animation(.easeInOut, value:labelNumber)
+            
+            Text("Graj")
+                .font(.system(size: labelNumber == 2 ? 28 : 14))
+                .fontWeight(labelNumber == 2 ? .bold : .regular)
+                .animation(.easeInOut, value:labelNumber)
+            
+            Spacer()
+            
+            Text("Dalej")
+                .frame(maxWidth: .infinity, alignment: .center)
+                .foregroundColor(.blue)
+                .fontWeight(.bold)
+                .font(.system(size: 30))
+                .onTapGesture {
+                    nextText()
+                }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+    }
+    
+    func nextText() {
+        labelNumber += 1
     }
 }
 
