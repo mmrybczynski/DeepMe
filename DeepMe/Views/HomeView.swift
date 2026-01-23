f4eb1becde11dc5f5ff49c512c44d2407bc9e563
Mateusz <mateusz.rybczynski@icloud.com>
2026-01-23 12:50:52 +0100

    In future update it will be main view

--- DeepMe/Views/HomeView.swift
+++ DeepMe/Views/HomeView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @StateObject private var questionStore = QuestionStore()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 30) {
+                    
+                    HStack(alignment: .center,spacing: 30) {
+                        Card(category: "simple", image: "simple", title: "Podstawowe", multipleCategory: false)
+                        Card(category: "party", image: "party", title: "Imprezowe", multipleCategory: false)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
+                    
+                    HStack(alignment: .center,spacing: 30) {
+                        Card(category: "cienie", image: "ghost", title: "Cienie", multipleCategory: false)
+                        Card(category: "wyznania", image: "door", title: "Wyznania", multipleCategory: false)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
+                    
+                    HStack(alignment: .center,spacing: 30) {
+                        Card(category: "dlapar", image: "loveheart", title: "Dla par", multipleCategory: false)
+                        Card(image: "skull", multipleCategory: true)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
+                }
+                .padding()
+            }
+            .frame(width: 360)
+            .navigationTitle(LocalizedStringKey("kategorie"))
+            .navigationBarTitleDisplayMode(.automatic)
+
+        }
     }
 }
 
--- DeepMe/Views/MainTabView.swift
+++ DeepMe/Views/MainTabView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            SettingsView()
+                .tabItem {
+                    Label("Ustawienia", systemImage: "gearshape.fill")
+                }
+        }
     }
 }
 
