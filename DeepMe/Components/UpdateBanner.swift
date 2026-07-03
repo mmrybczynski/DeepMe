c37ffefe0d0b52c421b6779d4b31a347713123f3
Mateusz <mateusz.rybczynski@icloud.com>
2026-07-03 08:51:39 +0200

    Created updateBanner

--- DeepMe/Components/UpdateBanner.swift
+++ DeepMe/Components/UpdateBanner.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct UpdateBanner: View {
+    @Environment(\.openURL) private var openURL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image("AppIconToBaner")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .cornerRadius(20)
+                .padding(.trailing, 10)
+            
+            VStack(alignment: .leading){
+                Text("DeepMe")
+                    .fontWeight(.bold)
+                Text("Dostępna nowa wersja")
+                    .font(.system(size: 12))
+                    .foregroundStyle(Color.gray)
+                
+            }
+            
+            Spacer()
+            
+            Button {
+                Task {
+                    if let url = URL(string: "https://apps.apple.com/pl/app/deepme/id6741001876") {
+                        openURL(url)
+                    }
+                }
+            } label: {
+                Text("Zaktualizuj")
+                    .foregroundStyle(Color.black)
+                    .font(.system(size: 14))
+            }
+        }
+        .padding()
+        .background(.thinMaterial)
+        .cornerRadius(20)
+        .padding()
+        
     }
 }
 
